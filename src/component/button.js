451e6e01c54a26dfcd494d863f3c7621e003a3e8
vairavan10 <vairavan1092003@gmail.com>
2025-02-27 19:27:15 +0530

    button.js modified

--- src/component/button.js
+++ src/component/button.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
+
 export default function BasicButtons({onClick}) {
   return (
     <div class="button-container">
